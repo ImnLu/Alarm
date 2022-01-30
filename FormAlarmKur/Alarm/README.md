899ad81268d889825bc356ef0729696db6ed3182
Mali <37624905+ImnLu@users.noreply.github.com>
2022-01-31 00:52:35 +0300

    Add files via upload

---
+++ FormAlarmKur/Alarm/README.md
@@ -0,0 +1,2 @@
+# Alarm
+ C# dilinde çalışan basit bir alarm. Henüz kodlara açıklama eklemedim ve çok bir özelliği yok. Zamanla geliştirmeyi düşünüyorum.
---
+++ FormAlarmKur/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;//Yeni kütüphane
+
+namespace FormAlarmKur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WMPLib.WindowsMediaPlayer alarm = new WMPLib.WindowsMediaPlayer();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (numericUpDown1.Value == DateTime.Now.Hour && numericUpDown2.Value == DateTime.Now.Minute)
+            {
+                timer1.Stop();
+                alarm.URL = "alarm.mp3";
+                alarm.controls.play();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            label1.Text = DateTime.Now.ToShortTimeString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            alarm.controls.stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToShortTimeString();
+        }
+    }
+}
